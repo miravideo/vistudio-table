e3654dde8bb8fd2e59b1fa2357cae35c7c230a29
Zhao Jun <leo.zhaojun@gmail.com>
2022-09-11 09:43:57 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App, getData, reloadData } from "./App.js";
+import { App } from "./App.js";
 import Store from "./store";
 
 const VERSION = '$VERSION';
