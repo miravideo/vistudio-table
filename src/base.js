6f242852205f996990d8d35f2f129b897b038d52
a6037220 <a6037220@163.com>
2023-04-09 19:40:15 +0800

    fix: 读取图片URL

--- src/base.js
+++ src/base.js
@@ -1,11 +1,22 @@
 import EventEmitter from "eventemitter3";
 
+const isImageUrl = (url) => {
+  return /\.(jpg|jpeg|png|gif|bmp)$/i.test(url);
+}
+
 export class Cell extends EventEmitter {
   constructor(opt) {
     super()
-    const {value, file, type} = opt
+    let {value, file, type} = opt
     this.value = value;
     this.file = file;
+
+    if (!type && isImageUrl(this.value)) {
+      type = 'image'
+    }
+
     this.type = type || 'text';
+
+
   }
 }
